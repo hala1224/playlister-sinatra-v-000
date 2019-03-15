7adbd89f052ad36d4dae3ed0f0d8e0e4f986431f
Natalie Halawani <hala1224@gmail.com>
2019-03-15 00:08:24 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190315000704) do
+ActiveRecord::Schema.define(version: 20190315000801) do
 
   create_table "artists", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "genres", force: :cascade do |t|
     t.string "name"
   end
 
