134b1dd4391dc9a5c947eec37059dbadfb205ed9
Natalie Halawani <hala1224@gmail.com>
2019-03-15 02:19:40 +0000

    Automatically backed up by Learn

--- db/migrate/20190315021541_create_genres.rb
+++ db/migrate/20190315021541_create_genres.rb
@@ -1,7 +1,8 @@
 class CreateGenres < ActiveRecord::Migration
   def change
-    create_table :genres do
-      t.string :name 
+    create_table :song_genres do |t|
+      t.integer :song_id
+      t.integer :genre_id
     end
   end
 end
